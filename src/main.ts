7ad8e1a92180c3de6e98755f92846c92dbc64070
kimsuyeon <suyeon3715@bitbucket.org>
2025-06-20 15:35:31 +0900

    feat: env

--- src/main.ts
+++ src/main.ts
@@ -2,16 +2,23 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   
-  // CORS 설정
+
   app.enableCors({
-    origin: ['http://localhost:3000'], // React 앱 URL
+    origin: [
+      'http://localhost:3000',
+      'https://settlement-frontend-production.up.railway.app'
+    ],
     credentials: true,
   });
+  
 
   // Validation Pipe 글로벌 설정
   app.useGlobalPipes(new ValidationPipe());
@@ -36,7 +43,8 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api-docs', app, document);
 
-  await app.listen(3001);
+  await app.listen(process.env.PORT || 3001);
+  
   console.log('백엔드 서버가 http://localhost:3001에서 실행중입니다.');
   console.log('Swagger 문서: http://localhost:3001/api-docs');
 }
